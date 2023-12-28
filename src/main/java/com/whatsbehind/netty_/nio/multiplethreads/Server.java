b569a64db84ee4138513068209aba7c8c2685c12
puyanh <puyanh18@gmail.com>
2023-12-28 09:44:24 -0800

    initial code of boss and worker

---
+++ src/main/java/com/whatsbehind/netty_/nio/multiplethreads/Server.java
@@ -0,0 +1,40 @@
+package com.whatsbehind.netty_.nio.multiplethreads;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.*;
+import java.util.Iterator;
+
+@Slf4j
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        Thread.currentThread().setName("boss");
+
+        // Create a ServerSocketChannel
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.configureBlocking(false);
+        ssc.bind(new InetSocketAddress(9999));
+
+        // Create a boss selector to listen to ACCEPT event from the ServerSocketChannel
+        Selector boss = Selector.open();
+        SelectionKey bossKey = ssc.register(boss, 0, null);
+        bossKey.interestOps(SelectionKey.OP_ACCEPT);
+
+        while (true) {
+            boss.select();
+            Iterator<SelectionKey> keyIterator = boss.selectedKeys().iterator();
+            while (keyIterator.hasNext()) {
+                SelectionKey key = keyIterator.next();
+                keyIterator.remove();
+                if (key.isAcceptable()) {
+                    ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                    SocketChannel sc = server.accept();
+                    log.debug("Accept client [{}] connection", sc.getRemoteAddress());
+                }
+            }
+        }
+    }
+}
